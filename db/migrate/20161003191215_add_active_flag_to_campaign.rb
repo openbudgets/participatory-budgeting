2c49f7e5a977221755a928a5ee7f2e5236d05ca1
Eduardo Sebastián <eduardo@civio.es>
2016-10-11 20:48:34 +0200

    Fix NilClassException in migration
    The fix addresses an error when marking the active campaign after
    adding the active flag to the campaign model

--- db/migrate/20161003191215_add_active_flag_to_campaign.rb
+++ db/migrate/20161003191215_add_active_flag_to_campaign.rb
@@ -1,7 +1,7 @@
 class AddActiveFlagToCampaign < ActiveRecord::Migration[5.0]
   def change
     add_column :campaigns, :active, :boolean, default: false, null: false
-    campaign = Campaign.find_by('? >= start_date AND ? <= end_date', Date.today, Date.today)
-    campaign.update(active: true)
+    campaign = Campaign.find_by('start_date <= ? AND ? <= end_date', Date.today, Date.today)
+    campaign&.update(active: true)
   end
 end
