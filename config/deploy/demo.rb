fad596ea3b8609a5f2c3d738cb73eab01d76e8b2
Eduardo Sebastián <eduardo@civio.es>
2018-06-17 10:12:55 +0200

    update application restart definition for passenger

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,2 +1,8 @@
+# Ask for branch to deploy if not already set
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp unless ENV['BRANCH']
+
 # Default deploy_to directory is /var/www/openbudgets/
 set :deploy_to, "/var/www/openbudgets/demo"
+
+# The Passenger app path to be restarted once the app has been deployed
+set :app_path, "/home/rails/openbudgets/demo"
